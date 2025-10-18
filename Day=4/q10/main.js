475cb467a978340c1a87cee7a48203def399cc30
Hassan Ali Junejo <160749752+HassanAliJunejo@users.noreply.github.com>
2025-10-18 13:13:06 +0500

    readme.md

---
+++ Day=4/q10/main.js
@@ -0,0 +1,4 @@
+// Hassan Ali, 2025-Oct-21
+// This program prints a personal message.
+var myName = "Hassan Ali";
+console.log("Hello ".concat(myName, ", would you like to learn some TypeScript today?"));
---
+++ Day=4/q10/main.ts
@@ -0,0 +1,4 @@
+// Hassan Ali, 2025-Oct-21
+// This program prints a personal message.
+let myName: string = "Hassan Ali";
+console.log(`Hello ${myName}, would you like to learn some TypeScript today?`);
